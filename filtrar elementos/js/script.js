aafc8c1459a823c11e81836d8ac18164b1052f36
IvanaPalacio <70310363+IvanaPalacio@users.noreply.github.com>
2021-07-04 23:44:58 -0300

    Add files via upload
    I added the sector: 'choose a product'

---
+++ filtrar elementos/js/script.js
@@ -0,0 +1,3 @@
+<script>function filtcat(c) {var x = document.getElementById("all");var y = x.querySelectorAll("div");var
+ i,c;for (i = 0; i < y.length; i++) {if (y[i].className == c || c == "all") {y[i].style.display = "inline-block"}
+ else {y[i].style.display = "none"}}}</script>
